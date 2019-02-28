0adcbd51170675a3864835d4a40260e0343d53e0
Krancki <kr.przygocki@gmail.com>
2019-03-01 00:00:33 +0100

    Update

--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
 let workBtn;
-let currentState = {
+let bustContent;
+const currentState = {
     'activeTabs': [],
 
 };
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
+    bustContent = document.getElementById("bustContent");
 
-    workBtn = this.getElementById("workBtn");
     //workBtn.addEventListener("click", sendData);
 });
 
@@ -22,17 +23,53 @@ const sendData = ()=>{
 };
 */
 
-chrome.runtime.onMessage.addListener(
-    function (request, sender, sendResponse) {
 
-        if (sender.tab) {
-            if(!currentState.activeTabs.includes(sender.tab.id)){
-                console.log(request.broadcast);
-                if(request.broadcast){
-                currentState.activeTabs.push(sender.tab.id);
+createBustElement =({id})=>{
+
+    const bustBlock= document.createElement("div");
+    const checkboxA= document.createElement("input");
+    const checkboxB= document.createElement("input");
+    const workBtn= document.createElement("input");
+    const bustInfo = document.createElement("p");
+    const removeBlock = document.createElement("input");
+
+    bustBlock.setAttribute("class","bust_block");
+    bustInfo.setAttribute("class","bustInfo");
+
+    checkboxA.setAttribute("type","checkbox");
+    checkboxB.setAttribute("type","checkbox");
+    workBtn.setAttribute("type","button");
+    removeBlock.setAttribute("type","button");
+
+    checkboxA.setAttribute("value","armyA");
+    checkboxB.setAttribute("value","armyB");
+    workBtn.setAttribute("value","On");
+
+    bustBlock.appendChild(checkboxA);
+    bustBlock.appendChild(checkboxB);
+    bustBlock.appendChild(workBtn);
+    bustBlock.appendChild(bustInfo);
+    bustBlock.appendChild(removeBlock);
+
+    bustContent.appendChild(bustBlock);
+};
+
+chrome.runtime.onMessage.addListener(
+    function ({broadcast}, {tab}) {
+
+        if (tab) {
+            if(!currentState.activeTabs.includes(tab.id)){
+                console.log(broadcast);
+                if(broadcast){
+                currentState.activeTabs.push(tab.id);
+                    if(!currentState.activeTabs.includes(tab.id)){
+                        currentState.activeTabs.push(tab.id);
+                        console.log(currentState);
+                        createBustElement(tab.id);
+                    }
                 }else{
                     console.log("remove");
-                   let index = currentState.activeTabs.indexOf(sender.tab.id);
+                   let index = currentState.activeTabs.indexOf(tab.id);
                    currentState.activeTabs.splice(index,1);
                 }
             }
@@ -42,19 +79,5 @@ chrome.runtime.onMessage.addListener(
     });
 
 
-findAllOpenTabs=()=>{
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {'bustScan': true}, function(response) {
-            if (chrome.runtime.lastError) {
-            console.log("There is no tabs");
-            }else if( response&& response.currentTab ) {
-                if(!currentState.activeTabs.includes(response.currentTab.id)){
-                    currentState.activeTabs.push(response.currentTab.id);
-                    console.log(currentState);
-                }
-            }
-        });
-    });
 
-}
