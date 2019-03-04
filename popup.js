3aa9b5c5eeb029b5cf681c43216274fee416784d
Krancki <kr.przygocki@gmail.com>
2019-03-04 19:03:35 +0100

    create element for each tab

--- popup.js
+++ popup.js
@@ -8,8 +8,9 @@ const currentState = {
 document.addEventListener("DOMContentLoaded", function (event) {
 
     bustContent = document.getElementById("bustContent");
+    refreshBtn = document.getElementById("refreshBtn");
+    refreshBtn.addEventListener('click',createBustElement);
 
-    //workBtn.addEventListener("click", sendData);
 });
 
 
@@ -24,14 +25,27 @@ const sendData = ()=>{
 */
 
 
-createBustElement =({id})=>{
+
+createBustElement =()=>{
+ console.log("create");
+    while(bustContent.firstChild){
+        bustContent.removeChild(bustContent.firstChild);
+    }
+    currentState.activeTabs.forEach(value => {
 
     const bustBlock= document.createElement("div");
+
+    const textA= document.createElement('span');;
     const checkboxA= document.createElement("input");
+    const textB = document.createElement('span');
     const checkboxB= document.createElement("input");
     const workBtn= document.createElement("input");
     const bustInfo = document.createElement("p");
     const removeBlock = document.createElement("input");
+
+    textA.innerText="A";
+    textB.innerText="B";
+
 
     bustBlock.setAttribute("class","bust_block");
     bustInfo.setAttribute("class","bustInfo");
@@ -40,18 +54,25 @@ createBustElement =({id})=>{
     checkboxB.setAttribute("type","checkbox");
     workBtn.setAttribute("type","button");
     removeBlock.setAttribute("type","button");
+    removeBlock.setAttribute("value","remove");
+
+    removeBlock.addEventListener('click',()=>{
+        bustBlock.remove();
+    });
 
     checkboxA.setAttribute("value","armyA");
     checkboxB.setAttribute("value","armyB");
     workBtn.setAttribute("value","On");
-
+    bustBlock.appendChild(textA);
     bustBlock.appendChild(checkboxA);
+    bustBlock.appendChild(textB);
     bustBlock.appendChild(checkboxB);
     bustBlock.appendChild(workBtn);
     bustBlock.appendChild(bustInfo);
     bustBlock.appendChild(removeBlock);
 
     bustContent.appendChild(bustBlock);
+    });
 };
 
 chrome.runtime.onMessage.addListener(
